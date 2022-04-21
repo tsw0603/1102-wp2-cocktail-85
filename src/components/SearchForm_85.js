f53a5382ecd051b7d4575cb30ee4f2db21196e0b
tsw0603 <209410785@gms.tku.edu.tw>
2022-04-21 21:34:57 +0800

    P5 Show Cocktails using searchTerm

--- src/components/SearchForm_85.js
+++ src/components/SearchForm_85.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { useGlobalContext } from '../context_85'
 
 const SearchForm_85 = () => {
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = useRef('');
+
+  useEffect(()=> {
+    searchValue.current.focus();
+  },[]);
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <section className='section search'>
-      <form className='search-form'>
+      <form className='search-form' onSubmit={handleSubmit}>
         <div className='form-control'>
           <label htmlFor='name'>search your favorite cocktail</label>
           <input
             type='text'
             name='name'
             id='name'
+            ref={searchValue}
+            onChange={searchCocktail}
           />
         </div>
       </form>
